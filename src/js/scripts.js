0b49931b4657ed7ef59935d85095676ad151ef11
inferi0n <rda@apptech-team.ru>
2019-06-06 15:17:51 +0300

    feature: created services page, accordion and slider fixes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,8 +2,17 @@ let prevScroll;
 
 $(document).ready(() => {
   objectFitImages()
+  initAccordions()
   initSliders()
 })
+
+function initAccordions() {
+  var windowWidth = $(window).width();
+  if (windowWidth > 768) {
+    $('.accordion__toggle').attr('aria-expanded', 'true')
+    $('.accordion__content').addClass('show')
+  }
+}
 
 
 function initMobileNav() {
@@ -39,6 +48,7 @@ function initSliders() {
         '<use xlink:href="img/icons.svg#arrow-right"></use>' +
         '</svg></button>',
         slidesToShow: 1,
+        initialSlide: 1,
         slidesToScroll: 1,
         variableWidth: false,
         centerMode: false,
@@ -50,6 +60,7 @@ function initSliders() {
             breakpoint: 767,
             settings: {
               slidesToShow: 1,
+              initialSlide: 1,
               arrows: false,
               dots: true,
               centerMode: true,
